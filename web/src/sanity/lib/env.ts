b0e78d9ea18253caa7408feba582f92e4c9ecd97
jonathanbraaten <jonathan@mjos.no>
2024-10-18 00:52:58 +0200

    add api version to env

--- web/src/sanity/lib/env.ts
+++ web/src/sanity/lib/env.ts
@@ -1,3 +1,4 @@
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const apiVersion = process.env.NEXT_PUBLIC_API_VERSION;
 export const token = process.env.SANITY_API_READ_TOKEN;
