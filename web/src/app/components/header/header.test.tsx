03c34896a9aa6ea4ca92df762a599a400531c23f
jonathanbraaten <jonathan@mjos.no>
2024-10-18 23:37:14 +0200

    fix yml

--- web/src/app/components/header/header.test.tsx
+++ web/src/app/components/header/header.test.tsx
@@ -3,7 +3,7 @@ import { describe } from 'node:test';
 import Header from '.';
 
 describe('Header', () => {
-  it('Should render Header component', () => {
+  it('Should render Header', () => {
     render(<Header />);
     const header = screen.getByRole('banner');
     expect(header).toBeInTheDocument();
