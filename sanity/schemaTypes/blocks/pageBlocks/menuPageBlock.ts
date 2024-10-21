89c05cfc828f7035901f153d10c9eb0d95395b2f
dreamCode <czon.herald75@gmail.com>
2024-10-21 13:04:05 +0545

    feat: About, CTA & Menu homepage section

---
+++ sanity/schemaTypes/blocks/pageBlocks/menuPageBlock.ts
@@ -0,0 +1,40 @@
+import {defineField, defineType} from 'sanity'
+import {PiLego} from 'react-icons/pi'
+
+export default defineType({
+  name: 'menuPageBlock',
+  type: 'object',
+  icon: PiLego,
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      description: 'Title for menu section',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Description for menu section',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      description: 'Image for menu section',
+      fields: [
+        defineField({
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+          description: 'Alternative text for screen reader',
+          validation: (Rule) => Rule.required(),
+        }),
+      ],
+    }),
+    defineField({name: 'linkEmbed', type: 'linkEmbed'}),
+  ],
+})
