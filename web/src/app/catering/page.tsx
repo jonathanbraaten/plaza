4e0510edbeca9ccbde1567c3389e03cba246d1ea
jonathanbraaten <jonathan@mjos.no>
2024-10-25 13:19:06 +0200

    add catering page

---
+++ web/src/app/catering/page.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/header';
+import Footer from '../components/footer';
+import HeroBanner from '../components/heroBanner';
+import cateringPageHandler from '@/components/pageBlock/catering';
+export default async function Page() {
+  return (
+    <>
+      <Header />
+      <main>cat</main>
+      <Footer />
+    </>
+  );
+}
---
+++ web/src/components/pageBlock/catering/index.tsx
@@ -0,0 +1,3 @@
+export default function cateringPageHandler() {
+  return <h1>Catering</h1>;
+}
