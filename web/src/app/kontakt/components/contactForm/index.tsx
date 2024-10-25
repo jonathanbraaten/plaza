e7a5a38a34f0a237875daf3791707074dcda2e02
jonathanbraaten <jonathan@mjos.no>
2024-10-26 00:32:18 +0200

    add contact form

---
+++ web/src/app/kontakt/components/contactForm/index.tsx
@@ -0,0 +1,80 @@
+'use client';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import FormInput from '@/app/components/formInput';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { contactSchema } from './contactSchema';
+export type Inputs = {
+  name: string;
+  email: string;
+  telephone: string;
+  message: string;
+};
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: zodResolver(contactSchema),
+    reValidateMode: 'onChange',
+  });
+  const message = watch('message');
+  const messageCount = message?.length || 0;
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log('clicked');
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
+      <div className="flex flex-col gap-3 w-full">
+        <FormInput
+          htmlFor="name"
+          labelText="Navn"
+          id="name"
+          name="name"
+          type="text"
+          optional={false}
+          errors={errors}
+          register={register}
+        />
+        <FormInput
+          htmlFor="email"
+          labelText="E-postadresse"
+          id="email"
+          name="email"
+          type="text"
+          optional={false}
+          errors={errors}
+          register={register}
+        />
+
+        <FormInput
+          htmlFor="telephone"
+          labelText="Telefon"
+          id="telephone"
+          name="telephone"
+          type="tel"
+          optional={true}
+          errors={errors}
+          register={register}
+        />
+        <FormInput
+          htmlFor="message"
+          labelText="Beskjed"
+          id="message"
+          name="message"
+          type="message"
+          optional={false}
+          errors={errors}
+          register={register}
+          msgLength={messageCount}
+        />
+      </div>
+      <button type="submit" className="self-start bg-primary text-white py-2 px-20 rounded-md">
+        Send
+      </button>
+    </form>
+  );
+}
---
+++ web/src/app/kontakt/page.tsx
@@ -0,0 +1,21 @@
+import Footer from '../components/footer';
+import Header from '../components/header';
+import Wrapper from '../components/wrapper';
+import ContactForm from './components/contactForm';
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <main className="flex justify-center items-center px-20">
+        <section className="max-w-[45rem] w-full">
+          <Wrapper optionalStyle="text-center">
+            <h1 className="text-mobile-h1 md:text-mobile-h2">Kontakt oss</h1>
+            <ContactForm />
+          </Wrapper>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
