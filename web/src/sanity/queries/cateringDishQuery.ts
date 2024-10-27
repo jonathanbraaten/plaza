aeae00dca3bd2829d3c8f4a8540eec895b685ba0
jonathanbraaten <jonathan@mjos.no>
2024-10-27 23:39:56 +0100

    fix catering

--- web/src/sanity/queries/cateringDishQuery.ts
+++ web/src/sanity/queries/cateringDishQuery.ts
@@ -1,6 +1,6 @@
 import { defineQuery } from 'next-sanity';
 
-export const CATERING_DISH_QUERY = defineQuery(`*[_type == 'cateringDishes']{
+/* export const CATERING_DISH_QUERY = defineQuery(`*[_type == 'cateringDishes']{
   _id,
   title,
   allergy,
@@ -8,3 +8,16 @@ export const CATERING_DISH_QUERY = defineQuery(`*[_type == 'cateringDishes']{
   pricePerPerson
 
 }`);
+ */
+export const CATERING_DISH_QUERY = defineQuery(`
+
+  *[_type == 'cateringDish'].dish[0].catering[]{
+    _key,
+  _id,
+  title,
+  allergy,
+  pricePerPerson,
+  description,
+  _type
+}
+  `);
