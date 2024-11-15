d1c9b6e4f89179f8a933c6ca41478d6e3975b117
jonathanbraaten <jonathan@mjos.no>
2024-11-15 10:03:00 +0100

    remove catering

--- sanity/schemaTypes/pageBlocks/index.ts
+++ sanity/schemaTypes/pageBlocks/index.ts
@@ -2,14 +2,7 @@ import aboutPageBlock from './introPageBlock';
 import CTAPageBlock from './ctaPageBlock';
 import menuPageBlock from './menuPageBlock';
 import dishHandler from './dishHandler';
-import cateringPageBlock from './cateringPageBlock';
+
 import cateringHandler from './cateringHandler';
 
-export default [
-  aboutPageBlock,
-  CTAPageBlock,
-  menuPageBlock,
-  cateringPageBlock,
-  dishHandler,
-  cateringHandler,
-];
+export default [aboutPageBlock, CTAPageBlock, menuPageBlock, dishHandler, cateringHandler];
